72637edc0d138e25191b072ba2930cf19b5aca24
Natália S. Gomes <nataliandsg32@gmail.com>
2024-05-10 17:47:36 -0300

    feat: add modelagem dos dados

---
+++ src/main/java/com/nataliatsi/tabelafipe/model/Dados.java
@@ -0,0 +1,7 @@
+package com.nataliatsi.tabelafipe.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Dados(String codigo, String nome) {
+}
